bfdf57121cf428ac850c2e5a56de6dc15661d214
gundeeep <gundeeep@gmail.com>
2025-10-29 15:37:25 +0530

    Update: New theme

--- app/components/Home/Affordable.tsx
+++ app/components/Home/Affordable.tsx
@@ -1,70 +1,53 @@
 import React from "react";
-import contactInfo from "@/components/Content/ContactInfo.json";
 import { GrUserWorker } from "react-icons/gr";
 import { FaRegThumbsUp } from "react-icons/fa";
 import { GiReceiveMoney } from "react-icons/gi";
 
+import contactContent from "@/app/Data/content";
+
+const ContactInfo: any = contactContent.contactContent;
+const home: any = contactContent.homePageContent;
+
 const Affordable = () => {
+  const content = home?.affordableWidget;
   return (
-    <div className="bg-gray-100 px-6 py-12 ">
+    <div className="bg-gray-100 px-6 py-12">
       <div className="mx-auto flex h-full max-w-7xl flex-col items-center justify-center gap-10 text-center lg:flex-row">
         <div className="lg:w-[40%]">
-          <h2 className="text-3xl font-bold text-gray-800">
-          Professional Water Heater Installation & Repair in Syracuse, NY
-          </h2>
-            <p className="mt-4 text-gray-600">
-            Need plumbing services in Syracuse? Syracuse Drain Cleaning Pros offers fast,
-            reliable, and affordable plumbing solutions. Whether it’s for
-            a leaky faucet, water heater installation, or major repairs, we have
-            the expertise to handle your needs. Call now and get your plumbing
-            issues resolved promptly!
-            </p>
-          <a id="cta-id" href={`tel:${contactInfo.tel}`}>
+          <h2 className="text-3xl font-bold text-gray-800">{content.title}</h2>
+          <p className="mt-4 text-gray-600">{content.description}</p>
+          <a id="cta-id" href={`tel:${ContactInfo.tel}`}>
             <button
               id="cta-id"
               className="mt-8 rounded-lg bg-minor px-6 py-3 text-white transition duration-700 ease-in hover:bg-main"
             >
-              Call Now
+              {content.ctaText}
             </button>
           </a>
         </div>
-        <div className="mt-8 flex flex-col justify-center gap-6 lg:flex-row ">
-          {/* Affordable Pricing */}
-          <div className="flex w-full max-w-sm flex-col items-center rounded-lg bg-white p-6 shadow-md ">
-            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-minor/20 ">
-              <GiReceiveMoney className=" text-xl" />
+        <div className="mt-8 flex flex-col justify-center gap-6 lg:flex-row">
+          {content.cards.map((card:any, index:number) => (
+            <div
+              key={index}
+              className={`flex w-full max-w-sm flex-col items-center rounded-lg ${
+                index === 1 ? "border border-main" : "bg-white"
+              } p-6 shadow-md`}
+            >
+              <div
+                className={`flex h-12 w-12 items-center justify-center rounded-full ${
+                  index === 1 ? "bg-green-100" : "bg-minor/20"
+                }`}
+              >
+                {index === 0 && <GiReceiveMoney className="text-xl" />}
+                {index === 1 && <GrUserWorker className="text-xl text-main" />}
+                {index === 2 && <FaRegThumbsUp className="text-xl" />}
+              </div>
+              <h3 className="mt-4 text-lg font-bold text-gray-800">
+                {card.title}
+              </h3>
+              <p className="mt-2 text-gray-600">{card.description}</p>
             </div>
-            <h3 className="mt-4 text-lg font-bold text-gray-800">
-              Affordable Plumbing Services
-            </h3>
-            <p className="mt-2 text-gray-600">
-              Syracuse Drain Cleaning Pros offers cost-effective plumbing solutions to fit any budget, ensuring you get the best value for your plumbing needs.
-            </p>
-          </div>
-          {/* Various Services */}
-          <div className="flex w-full max-w-sm flex-col items-center rounded-lg border border-main bg-white p-6 shadow-md">
-            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-              <GrUserWorker className="text-xl text-main" />
-            </div>
-            <h3 className="mt-4 text-lg font-bold text-gray-800">
-              Comprehensive Plumbing Services
-            </h3>
-            <p className="mt-2 text-gray-600">
-              From leaky faucets to major repairs, Syracuse Drain Cleaning Pros offers a wide range of plumbing services to meet your specific needs.
-            </p>
-          </div>
-          {/* Timely Service */}
-          <div className="flex w-full max-w-sm flex-col items-center rounded-lg bg-white p-6 shadow-md">
-            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-minor/20">
-              <FaRegThumbsUp className=" text-xl" />
-            </div>
-            <h3 className="mt-4 text-lg font-bold text-gray-800">
-              Prompt Service
-            </h3>
-            <p className="mt-2 text-gray-600">
-              Our team ensures prompt service, so your plumbing issues are resolved quickly and efficiently, keeping your home or business running smoothly.
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </div>
